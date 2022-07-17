f9a2433da56ad699a1a957f0624aae65167821b8
AbreuHD <abreumartinezjefferson@gmail.com>
2022-07-17 17:50:18 -0400

    Starting backend

---
+++ K-haku Backend/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace K_haku_Backend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
