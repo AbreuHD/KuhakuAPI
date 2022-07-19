78e069034db5fd826bf3250ca189103f36e3747d
AbreuHD <abreumartinezjefferson@gmail.com>
2022-07-19 14:35:43 -0400

    Nav Done

---
+++ K-haku Backend/Controllers/ScrapersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace K_haku_Backend.Controllers
+{
+    public class ScrapersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
