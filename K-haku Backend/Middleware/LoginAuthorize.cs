163566f6bf240c8cb0912d52a63f87f890f5aa70
AbreuHD <abreumartinezjefferson@gmail.com>
2022-08-22 12:23:08 -0400

    AccountController

---
+++ K-haku Backend/Middleware/LoginAuthorize.cs
@@ -0,0 +1,30 @@
+﻿using K_haku_Backend.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace K_haku.WebApp.Middleware
+{
+    public class LoginAuthorize : IAsyncActionFilter
+    {
+        private readonly ValidateUser _validateUser;
+        public LoginAuthorize(ValidateUser validateUser)
+        {
+            _validateUser = validateUser;
+        }
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (_validateUser.HasUser())
+            {
+                var controller = (AccountController)context.Controller;
+                context.Result = controller.RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                await next();
+            }
+        }
+    }
+}
