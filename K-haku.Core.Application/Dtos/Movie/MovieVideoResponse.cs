837e41d09c962955e466377693cd3a65ac65ff2a
AbreuHD <abreumartinezjefferson@gmail.com>
2022-09-14 19:04:00 -0400

    Updates

--- K-haku.Core.Application/Dtos/Movie/MovieVideoResponse.cs
+++ K-haku.Core.Application/Dtos/Movie/MovieVideoResponse.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace K_haku.Core.Application.ViewModels.Cuevana
+namespace K_haku.Core.Application.Dtos.Movie
 {
     public class MovieVideoResponse
     {
---
+++ K-haku.Core.Domain/Entities/Error.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K_haku.Core.Domain.Entities
+{
+    public class Error
+    {
+        public int ID { get; set; }
+        public string className { get; set; }
+        public string ErrorType { get; set; }
+        public string Info { get; set; }
+    }
+}
