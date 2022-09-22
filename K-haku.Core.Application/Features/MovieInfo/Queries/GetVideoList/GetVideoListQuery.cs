3f67cab86d7bb56b6d538d376b3385477468b491
AbreuHD <abreumartinezjefferson@gmail.com>
2022-09-22 18:00:03 -0400

    Some Things

--- K-haku.Core.Application/Features/MovieInfo/Queries/GetVideoList/GetVideoListQuery.cs
+++ K-haku.Core.Application/Features/MovieInfo/Queries/GetVideoList/GetVideoListQuery.cs
@@ -1,4 +1,5 @@
 ﻿using K_haku.Core.Application.Dtos.Movie;
+using K_haku.Core.Application.Interface.Repositories;
 using K_haku.Core.Application.WebsScrapers.GetVideos.Cuevana;
 using MediatR;
 using System;
@@ -10,15 +11,15 @@ using System.Threading.Tasks;
 
 namespace K_haku.Core.Application.Features.MovieInfo.Queries.GetVideoList
 {
-    public class GetVideoListQuery : IRequest<List<MovieVideoResponse>>
+    public class GetVideoListQuery : IRequest<List<MovieVideoResponse>> //List<MovieVideoResponse>>
     {
         public string MovieLink { get; set; }
     }
-    public class GetVideoListQueryHandler : IRequestHandler<GetVideoListQuery, List<MovieVideoResponse>>
+    public class GetVideoListQueryHandler : IRequestHandler<GetVideoListQuery, List<MovieVideoResponse>>//List<MovieVideoResponse>>
     {
         CuevanaGetAllVideos CuevanaVideo = new();
 
-        public Task<List<MovieVideoResponse>> Handle(GetVideoListQuery request, CancellationToken cancellationToken)
+        public Task<List<MovieVideoResponse>> Handle(GetVideoListQuery request, CancellationToken cancellationToken) //List<MovieVideoResponse>>
         {
             return CuevanaVideo.MovieVideos(request.MovieLink);
         }
