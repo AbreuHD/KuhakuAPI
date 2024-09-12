f5beeab307e1c59adcb8e2e092e932853723f115
Jefferson Abreu Martinez <abreumartinezjefferson@gmail.com>
2024-09-11 21:12:07 -0400

    ConfirmEmail and ResentConfirmation Added

--- Presentation.KuhakuCentral/Controllers/V1/Account/AccountController.cs
+++ Presentation.KuhakuCentral/Controllers/V1/Account/AccountController.cs
@@ -1,5 +1,7 @@
 ﻿using Auth.Core.Application.Features.Login.Queries.AuthLogin;
+using Auth.Infraestructure.Identity.Features.AuthenticateEmail.Command.AuthEmail;
 using Auth.Infraestructure.Identity.Features.Register.Commands.CreateAccount;
+using Auth.Infraestructure.Identity.Features.Register.Commands.SendValidationEmailAgain;
 using KuhakuCentral.Controllers.General;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -8,7 +10,7 @@ namespace KuhakuCentral.Controllers.V1.Account
 {
     public class AccountController(IMediator mediator, ILogger<AccountController> logger) : BaseAPI
     {
-        public IMediator Mediator { get; } = mediator;
+        public new IMediator Mediator { get; } = mediator;
         private readonly ILogger<AccountController> _logger = logger;
 
         [HttpPost("Login")]
@@ -20,6 +22,20 @@ namespace KuhakuCentral.Controllers.V1.Account
 
         [HttpPost("Register")]
         public async Task<IActionResult> Register([FromBody] CreateAccountCommand request)
+        {
+            var data = await Mediator.Send(request);
+            return Ok(data);
+        }
+
+        [HttpPost("ConfirmEmail")]
+        public async Task<IActionResult> ConfirmEmail([FromQuery] AuthEmailCommand request)
+        {
+            var data = await Mediator.Send(request);
+            return Ok(data);
+        }
+
+        [HttpPost("ResentConfirmation")]
+        public async Task<IActionResult> ResentConfirmation([FromBody] SendValidationEmailAgainCommand request)
         {
             var data = await Mediator.Send(request);
             return Ok(data);
