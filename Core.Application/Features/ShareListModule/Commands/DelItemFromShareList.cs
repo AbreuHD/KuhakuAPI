bbb28674b360a6bb534755e4777598541e7e8b27
Jefferson Abreu <abreumartinezjefferson@gmail.com>
2025-01-22 21:51:37 -0400

    Rename MovieListID to ShareListID and add share list features
    Renamed the MovieListID property to ShareListID across various files and updated related methods and database schema. Added new functionalities to handle user-specific share lists, including adding and deleting items with proper authorization checks. Introduced new endpoints in ShareListController to support these operations. Added a new migration file for the renaming changes in the database schema.

---
+++ Core.Application/Features/ShareListModule/Commands/DelItemFromShareList.cs
@@ -0,0 +1,73 @@
+﻿using Auth.Core.Application.DTOs.Generic;
+using Core.Application.Interface.Repositories;
+using Core.Domain.Entities.Relations;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Core.Application.Features.ShareListModule.Commands
+{
+    public class DelItemFromShareList : IRequest<GenericApiResponse<bool>>
+    {
+        [JsonIgnore]
+        public string? UserId { get; set; }
+        public required int ListId { get; set; }
+        public required int ItemId { get; set; }
+    }
+    public class DelItemFromShareListHandler : IRequestHandler<DelItemFromShareList, GenericApiResponse<bool>>
+    {
+        private readonly IMovieList_MovieRepository _movieList_MovieRepository;
+        private readonly IShareListRepository _shareListRepository;
+
+        public DelItemFromShareListHandler(IMovieList_MovieRepository movieList_MovieRepository, IShareListRepository shareListRepository)
+        {
+            _movieList_MovieRepository = movieList_MovieRepository;
+            _shareListRepository = shareListRepository;
+        }
+
+        public async Task<GenericApiResponse<bool>> Handle(DelItemFromShareList request, CancellationToken cancellationToken)
+        {
+            var response = new GenericApiResponse<bool>();
+
+            try
+            {
+                if (!await _movieList_MovieRepository.ItemExist(request.ItemId, request.ListId))
+                {
+                    response.Success = false;
+                    response.Statuscode = StatusCodes.Status500InternalServerError;
+                    response.Payload = false;
+                    response.Message = "Item not found in share list";
+                    return response;
+                }
+                var shareRequest = await _shareListRepository.GetByIdAsync(request.ListId);
+                if (shareRequest.UserID != request.UserId)
+                {
+                    response.Success = false;
+                    response.Statuscode = StatusCodes.Status401Unauthorized;
+                    response.Payload = false;
+                    response.Message = "You are not authorized to delete item from this list";
+                    return response;
+                }
+                await _movieList_MovieRepository.GetByIdAsync(request.ItemId);
+                response.Success = true;
+                response.Statuscode = StatusCodes.Status200OK;
+                response.Payload = true;
+                response.Message = "Item deleted from share list";
+            }
+            catch (Exception ex)
+            {
+                response.Success = false;
+                response.Statuscode = StatusCodes.Status500InternalServerError;
+                response.Payload = false;
+                response.Message = ex.Message;
+                return response;
+            }
+            return response;
+        }
+    }
+}
